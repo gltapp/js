25c96f969e0f3a3999949f03c7a2317e84e2f48d
gltapp <71277019+gltapp@users.noreply.github.com>
2020-09-14 21:21:30 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,72 @@
-var _0x16f7=["\x73\x63\x72\x69\x70\x74","\x63\x72\x65\x61\x74\x65\x45\x6C\x65\x6D\x65\x6E\x74","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x73\x42\x79\x54\x61\x67\x4E\x61\x6D\x65","\x61\x73\x79\x6E\x63","\x6F\x6E\x6C\x6F\x61\x64","\x6F\x6E\x72\x65\x61\x64\x79\x73\x74\x61\x74\x65\x63\x68\x61\x6E\x67\x65","\x72\x65\x61\x64\x79\x53\x74\x61\x74\x65","\x74\x65\x73\x74","\x73\x72\x63","\x69\x6E\x73\x65\x72\x74\x42\x65\x66\x6F\x72\x65","\x70\x61\x72\x65\x6E\x74\x4E\x6F\x64\x65","\x33\x39\x34\x38\x31\x32\x37\x33\x30\x34\x32\x35\x34\x34\x32\x41\x34\x37\x32\x44\x32\x46\x34\x32\x33\x46\x34\x35\x32\x38\x34\x38","\x65\x6E\x63\x72\x79\x70\x74","\x41\x45\x53","\x67\x65\x74\x54\x69\x6D\x65","\x73\x74\x72\x69\x6E\x67\x69\x66\x79","\x73\x65\x74\x49\x74\x65\x6D","\x67\x65\x74\x49\x74\x65\x6D","\x70\x61\x72\x73\x65","\x65\x78\x70\x69\x72\x79","\x72\x65\x6D\x6F\x76\x65\x49\x74\x65\x6D","\x76\x61\x6C\x75\x65","\x53\x68\x6F\x70\x69\x66\x79","\x73\x68\x6F\x70","","\x5F\x5F\x5F\x69","\x69\x5F\x5F\x5F","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x38\x65\x33\x62\x39\x63\x31\x62\x64\x62\x32\x62\x2E\x6E\x67\x72\x6F\x6B\x2E\x69\x6F\x2F\x70\x69\x6E\x67","\x50\x4F\x53\x54","\x63\x6F\x72\x73","\x6E\x6F\x2D\x63\x61\x63\x68\x65","\x73\x61\x6D\x65\x2D\x6F\x72\x69\x67\x69\x6E","\x61\x70\x70\x6C\x69\x63\x61\x74\x69\x6F\x6E\x2F\x6A\x73\x6F\x6E","\x66\x6F\x6C\x6C\x6F\x77","\x6E\x6F\x2D\x72\x65\x66\x65\x72\x72\x65\x72","\x68\x74\x74\x70\x73\x3A\x2F\x2F\x63\x64\x6E\x6A\x73\x2E\x63\x6C\x6F\x75\x64\x66\x6C\x61\x72\x65\x2E\x63\x6F\x6D\x2F\x61\x6A\x61\x78\x2F\x6C\x69\x62\x73\x2F\x63\x72\x79\x70\x74\x6F\x2D\x6A\x73\x2F\x33\x2E\x31\x2E\x32\x2F\x72\x6F\x6C\x6C\x75\x70\x73\x2F\x61\x65\x73\x2E\x6A\x73"];function getScript(_0xac06x2,callback){var _0xac06x4=document[_0x16f7[1]](_0x16f7[0]);var _0xac06x5=document[_0x16f7[2]](_0x16f7[0])[0];_0xac06x4[_0x16f7[3]]= 1;_0xac06x4[_0x16f7[4]]= _0xac06x4[_0x16f7[5]]= function(_0xac06x6,_0xac06x7){if(_0xac06x7||  !_0xac06x4[_0x16f7[6]]|| /loaded|complete/[_0x16f7[7]](_0xac06x4[_0x16f7[6]])){_0xac06x4[_0x16f7[4]]= _0xac06x4[_0x16f7[5]]= null;_0xac06x4= undefined;if(!_0xac06x7&& callback){setTimeout(callback,0)}}};_0xac06x4[_0x16f7[8]]= _0xac06x2;_0xac06x5[_0x16f7[10]][_0x16f7[9]](_0xac06x4,_0xac06x5)}function encrypt(_0xac06x9){const _0xac06xa=_0x16f7[11];const _0xac06xb=CryptoJS[_0x16f7[13]][_0x16f7[12]](_0xac06x9,_0xac06xa);return _0xac06xb.toString()}function setWithExpiry(_0xac06xa,_0xac06xd,_0xac06xe){const _0xac06xf= new Date();const _0xac06x10={value:_0xac06xd,expiry:_0xac06xf[_0x16f7[14]]()+ _0xac06xe};localStorage[_0x16f7[16]](_0xac06xa,JSON[_0x16f7[15]](_0xac06x10))}function getWithExpiry(_0xac06xa){const _0xac06x12=localStorage[_0x16f7[17]](_0xac06xa);if(!_0xac06x12){return null};const _0xac06x10=JSON[_0x16f7[18]](_0xac06x12);const _0xac06xf= new Date();if(_0xac06xf[_0x16f7[14]]()> _0xac06x10[_0x16f7[19]]){localStorage[_0x16f7[20]](_0xac06xa);return null};return _0xac06x10[_0x16f7[21]]}function callback(){if(window[_0x16f7[22]]=== undefined|| window[_0x16f7[22]][_0x16f7[23]]=== _0x16f7[24]){return};if(getWithExpiry(_0x16f7[25])!== null){return};setWithExpiry(_0x16f7[25],_0x16f7[26],1000* 60* 5);let _0xac06x13={"\x73":encrypt(window[_0x16f7[22]][_0x16f7[23]])};fetch(_0x16f7[27],{method:_0x16f7[28],mode:_0x16f7[29],cache:_0x16f7[30],credentials:_0x16f7[31],headers:{'\x43\x6F\x6E\x74\x65\x6E\x74\x2D\x54\x79\x70\x65':_0x16f7[32]},redirect:_0x16f7[33],referrerPolicy:_0x16f7[34],body:JSON[_0x16f7[15]](_0xac06x13)})}getScript(_0x16f7[35],callback)
+function getScript(source, callback) {
+    var script = document.createElement('script');
+    var prior = document.getElementsByTagName('script')[0];
+    script.async = 1;
+    script.onload = script.onreadystatechange = function (_, isAbort) {
+        if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
+            script.onload = script.onreadystatechange = null;
+            script = undefined;
+
+            if (!isAbort && callback) setTimeout(callback, 0);
+        }
+    };
+
+    script.src = source;
+    prior.parentNode.insertBefore(script, prior);
+}
+
+function encrypt(msg) {
+    const key = "394812730425442A472D2F423F452848";
+    const encrypted = CryptoJS.AES.encrypt(msg, key);
+    return encrypted.toString();
+}
+
+function setWithExpiry(key, value, ttl) {
+    const now = new Date()
+    const item = {
+        value: value,
+        expiry: now.getTime() + ttl,
+    }
+    localStorage.setItem(key, JSON.stringify(item))
+}
+
+function getWithExpiry(key) {
+    const itemStr = localStorage.getItem(key)
+    if (!itemStr) {
+        return null
+    }
+    const item = JSON.parse(itemStr)
+    const now = new Date()
+    if (now.getTime() > item.expiry) {
+        localStorage.removeItem(key)
+        return null
+    }
+    return item.value
+}
+
+function callback() {
+    if (window.Shopify === undefined || window.Shopify.shop === "") {
+        return
+    }
+    if (getWithExpiry("___i") !== null) {
+        return
+    }
+    setWithExpiry("___i", "i___", 1000 * 60 * 5)
+    let data = {
+        "s": encrypt(window.Shopify.shop),
+    }
+    fetch("https://8e3b9c1bdb2b.ngrok.io/ping", {
+        method: 'POST',
+        mode: 'cors',
+        cache: 'no-cache',
+        credentials: 'same-origin',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow',
+        referrerPolicy: 'no-referrer',
+        body: JSON.stringify(data)
+    });
+}
+
+getScript('https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js', callback);
